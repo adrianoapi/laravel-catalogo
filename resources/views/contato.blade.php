66750ddc68e8641912608732911d430a6c95166f
Adriano A Costa <sdcomputadores@gmail.com>
2017-09-24 13:14:24 -0300

    add estrutura

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,2 @@
+<h1><?= $titulo ?></h1>
+<a href="#">sdcomptuadores@gmail.com</a>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/contato', 'ContatoController');
+
+# Produto
+Route::get('/', 'ProdutosController@index');
+Route::post('contato/enviar', 'ContatoController@enviar');
+Route::resource('produtos', 'ProdutosController');
+Route::post('produtos/buscar', 'ProdutosController@buscar');
+Route::get('adicionar-produto', 'ProdutosController@create');
+Route::get('produtos/{id}/editar', 'ProdutosController@edit');
+Route::get('produtos/{id}/detalhes', 'ProdutosController@show');
+Route::get('extras-eloquent', 'ProdutosController@extras');
+Route::get('contato', 'ContatoController@index');
